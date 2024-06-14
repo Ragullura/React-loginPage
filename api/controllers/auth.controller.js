f5276e7559dc53a35e93f077a799d3c98eb4786d
Ragullura <ragullura.005@gmail.com>
2024-06-15 05:13:33 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model";
+import bcryptjs from 'bcryptjs';
+export const signup =async (req,res) =>{
+     const {username,email,password}=req.body;
+
+     if(!username || !email || !password || username==="" || email==="" || password===""){
+        return res.status(400).json({message:"Please fill all the fields"})
+     }
+
+     const hashedPassword =bcryptjs.hashSync(password,10);
+
+     const newUser =new User({
+        username,
+        email,
+        password:hashedPassword
+     });
+     try {
+         
+         await newUser.save(); 
+         res.json("User created successfully")
+     } catch (error) {
+        res.status(400).json("User already exists")
+     }
+
+}
